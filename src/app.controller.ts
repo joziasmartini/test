11021100af9137d6af9f624eae03afe7fbc4a5a5
joziasmartini <jozias.martini@gmail.com>
2025-05-13 14:09:31 -0300

    feat(api): add file upload endpoint with validation and processing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import validateFile from './fileValidator';
 
 @Controller()
 export class AppController {
@@ -14,10 +15,28 @@ export class AppController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    const data = file.buffer.toString();
+    const { isValid, errors } = validateFile(file);
 
-    const parsedHeader = data.split('\n');
+    if (!isValid) {
+      return {
+        statusCode: 400,
+        message: 'File is not valid',
+        errors,
+      };
+    }
 
-    this.appService.mathOperation(parsedHeader);
+    try {
+      this.appService.processTransactions(file);
+    } catch (error) {
+      return {
+        statusCode: 500,
+        message: 'Internal server error',
+        errors: [error.message],
+      };
+    }
+    return {
+      statusCode: 200,
+      message: 'File processed successfully',
+    };
   }
 }
