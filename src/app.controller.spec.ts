76b1ecdc5abbd4bfdca362090b953473fbe19f7f
joziasmartini <jozias.martini@gmail.com>
2025-05-13 14:10:33 -0300

    test(controller): add unit test setup for AppController with mock file

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Readable } from 'stream';
+
+const buffer = Buffer.from('from;to;amount\nA;B;100\nC;D;200');
+
+const mockFile: Express.Multer.File = {
+  fieldname: 'file',
+  originalname: 'filename.csv',
+  encoding: '7bit',
+  mimetype: 'text/csv',
+  size: buffer.length,
+  stream: Readable.from(buffer),
+  destination: '',
+  filename: '',
+  path: '',
+  buffer,
+};
 
 describe('AppController', () => {
   let appController: AppController;
@@ -16,7 +32,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.uploadFile(mockFile)).toBe('Hello World!');
     });
   });
 });
