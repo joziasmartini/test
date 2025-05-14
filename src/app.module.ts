b0f62738e8d0dbb1bb5d6e0ccac1bd5ea7f7f691
joziasmartini <jozias.martini@gmail.com>
2025-05-14 16:27:33 -0300

    feat(app): import prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UploadModule } from './upload/upload.module';
+import { PrismaModule } from './../prisma/prisma.module';
 
 @Module({
-  imports: [UploadModule],
+  imports: [UploadModule, PrismaModule],
 })
 export class AppModule {}
